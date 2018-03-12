4acf54ec8919a03237877835b635f6a0b3943eef
Eric Nograles <enograles@appirio.com>
2018-03-12 16:30:22 -0400

    adds home link for offline demo

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -6,50 +6,56 @@ import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
+import HomeIcon from 'material-ui-icons/Home';
 import connectOnly from '../redux/connectors/connectOnly';
 import { PROFILE_STATUS } from '../redux/reducers/profile';
+import { Link } from 'react-router-dom';
 
 const styles = {
   root: {},
   flex: {
-    flex: 1,
+    flex: 1
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20,
-  },
+    marginRight: 20
+  }
 };
 
 class TopAppBar extends React.Component {
   logout = () => {
     const { profileActions } = this.props;
     profileActions.logout();
-  }
+  };
 
   render() {
     const { classes, profile } = this.props;
 
     return (
-      profile.status === PROFILE_STATUS.AUTHENTICATED && 
-        <AppBar position="static">
+      profile.status === PROFILE_STATUS.AUTHENTICATED &&
+      <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
+          <Link to="/" style={{ textDecoration: 'none', color: '#ffffff' }}>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="home"
+            >
+              <HomeIcon />
+            </IconButton>
+          </Link>
           <Typography variant="title" color="inherit" className={classes.flex}>
             Orlando Code Camp 2018 GraphQL
           </Typography>
-          <Button color="inherit" onClick={this.logout}>Logout</Button>
+          <Button color="inherit" onClick={this.logout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
     );
   }
 }
 
-const enhance = compose(
-  connectOnly,
-  withStyles(styles)
-);
+const enhance = compose(connectOnly, withStyles(styles));
 
 export default enhance(TopAppBar);
